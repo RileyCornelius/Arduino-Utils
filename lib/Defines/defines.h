0c225b7c78a741015639a7f350ce5ff857ae298e
5and5 <54592152+RileyCornelius@users.noreply.github.com>
2022-12-22 19:03:28 -0600

    Add useful defines

--- lib/Defines/defines.h
+++ lib/Defines/defines.h
@@ -1,1 +1,9 @@
 #pragma once
+
+// return length of an array
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
+// no operation
+#define NOP() __asm__("nop");
+// Join two symbols together
+#define CONCAT(x, y) I_CONCAT(x, y)
+#define I_CONCAT(x, y) x##y
