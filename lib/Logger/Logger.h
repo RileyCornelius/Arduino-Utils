bed17c25b84e29e11bc69be16658c3e45286ef45
Riley Cornelius <54592152+RileyCornelius@users.noreply.github.com>
2024-11-12 22:42:07 -0600

    Remove old logger

--- lib/Logger/Logger.h
+++ lib/Logger/Logger.h
@@ -1,48 +1,168 @@
 #pragma once
 
-/**--------------------------------------------------------------------------------------
- * Settings
- *-------------------------------------------------------------------------------------*/
+#include <Arduino.h>
 
-// Min log level variable
-#define LOG_LEVEL_MIN LOG_LEVEL_DEBUG
+// Print << operator for logging
+// Example: LOG_DEBUG("TST", "var1" << var1 << ", var2=" << var2)
+template <class T>
+inline Print &operator<<(Print &obj, T arg)
+{
+    obj.print(arg);
+    return obj;
+}
 
-// Log level variable
-#define LOG_LEVEL_MAX LOG_LEVEL_ERROR
+#define endl "\r\n"
 
-// Time getter
-#define LOG_TIME_GETTER String(millis() / 1000).c_str()
-
-// Print time in message
-#define LOG_PRINT_TIME 0
-
-// Print log types full name or single letter
-#define LOG_PRINT_LONG_NAME 0
-
-/**--------------------------------------------------------------------------------------
- * Logger Defines
- *-------------------------------------------------------------------------------------*/
-
-#define LOG_LEVEL_NONE 0
-#define LOG_LEVEL_DEBUG 1
-#define LOG_LEVEL_INFO 2
-#define LOG_LEVEL_WARN 3
-#define LOG_LEVEL_ERROR 4
-
-// Log format defines
-#if LOG_LEVEL_MIN > 0
-
-#define LOG_DEBUG(tag, format, ...) iformat(LOG_LEVEL_DEBUG, tag, format, ##__VA_ARGS__)
-#define LOG_INFO(tag, format, ...) iformat(LOG_LEVEL_INFO, tag, format, ##__VA_ARGS__)
-#define LOG_WARN(tag, format, ...) iformat(LOG_LEVEL_WARN, tag, format, ##__VA_ARGS__)
-#define LOG_ERROR(tag, format, ...) iformat(LOG_LEVEL_ERROR, tag, format, ##__VA_ARGS__)
-#else
-#define LOG_DEBUG(format, ...)
-#define LOG_INFO(format, ...)
-#define LOG_WARN(format, ...)
-#define LOG_ERROR(format, ...)
+// Default to log to "Serial". But it can be defined in code to log to Serial2 for example.
+#ifndef LOG_OUTPUT
+#define LOG_OUTPUT Serial
 #endif
 
-// Prototypes functions
-void iformat(uint8_t level, const char *tag, const char *format, ...);
-void iformat(uint8_t level, const char *tag, String format, ...);
+// LOG levels order/priority. IF LOG_LEVEL_NONE is set, then nothing will be logged
+#define LOG_LEVEL_NONE -1
+#define LOG_LEVEL_ERROR 0
+#define LOG_LEVEL_WARNING 1
+#define LOG_LEVEL_NOTICE 2
+#define LOG_LEVEL_INFO 3
+#define LOG_LEVEL_DEBUG 4
+
+// Default LOG_LEVEL is set to LOG_LEVEL_DEBUG, if it's not defined in user code.
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_LEVEL_DEBUG
+#endif
+
+// Filter
+#define LOG_FILTER_INCLUDE 1
+#define LOG_FILTER_EXCLUDE 0
+
+#ifndef LOG_FILTER
+#define LOG_FILTER LOG_FILTER_EXCLUDE
+#endif
+
+#ifndef LOG_FILTER_LIST
+#define LOG_FILTER_LIST ""
+#endif
+
+// Timestamp
+#ifndef LOG_USE_TIME
+#define LOG_USE_TIME 0
+#endif
+
+// Log Level Tag
+#ifndef LOG_USE_TAG_SHORT
+#define LOG_USE_TAG_SHORT 0
+#endif
+
+// Log colors
+#ifndef LOG_COLORS
+#define LOG_COLORS 1
+#endif
+
+// Log functions
+
+#if LOG_LEVEL > LOG_LEVEL_NONE
+
+#if LOG_USE_TAG_SHORT
+static const char *__log_levels_text[] = {"[E]", "[W]", "[N]", "[I]", "[D]"};
+#else
+static const char *__log_levels_text[] = {"[EROR]", "[WARN]", "[NOTI]", "[INFO]", "[DBUG]"};
+#endif
+
+static char __log_time_format_buff[18];
+
+void format_time_hms()
+{
+#if LOG_USE_TIME
+    long ms = millis();
+    long seconds, minutes, hours, days;
+    seconds = ms / 1000;
+    minutes = seconds / 60;
+    hours = minutes / 60;
+    sprintf(__log_time_format_buff, "[%02u:%02u:%02u:%03u]", hours % 24, minutes % 60, seconds % 60, ms % 1000);
+#endif
+}
+
+bool can_log(const char *tag)
+{
+    bool in_filter = strstr(LOG_FILTER_LIST, tag) != NULL;
+    return LOG_FILTER ? in_filter : !in_filter; // INCLUDE or EXCLUDE
+}
+
+#define LOG_WRITE(loglevel, message)                                                                      \
+    format_time_hms();                                                                                    \
+    LOG_OUTPUT << __log_time_format_buff << " " << __log_levels_text[loglevel] << " " << message << endl; \
+    LOG_OUTPUT.flush();
+
+#define LOG_WRITE_TAG(loglevel, tag, message)                                                                                 \
+    if (can_log(tag))                                                                                                         \
+    {                                                                                                                         \
+        format_time_hms();                                                                                                    \
+        LOG_OUTPUT << __log_time_format_buff << " " << __log_levels_text[loglevel] << " [" << tag << "] " << message << endl; \
+        LOG_OUTPUT.flush();                                                                                                   \
+    }
+
+#endif // if LOG_LEVEL > LOG_LEVEL_NONE
+
+// Define logging macro functions per LOG_LEVEL
+
+#if LOG_LEVEL >= LOG_LEVEL_DEBUG
+#define LOG_DEBUG(tag, message) LOG_WRITE_TAG(LOG_LEVEL_DEBUG, tag, message)
+#else
+#define LOG_DEBUG(tag, message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_INFO
+#define LOG_INFO(tag, message) LOG_WRITE_TAG(LOG_LEVEL_INFO, tag, message)
+#else
+#define LOG_INFO(tag, message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_NOTICE
+#define LOG_NOTICE(tag, message) LOG_WRITE_TAG(LOG_LEVEL_NOTICE, tag, message)
+#else
+#define LOG_NOTICE(tag, message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_WARNING
+#define LOG_WARNING(tag, message) LOG_WRITE_TAG(LOG_LEVEL_WARNING, tag, message)
+#else
+#define LOG_WARNING(tag, message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_ERROR
+#define LOG_ERROR(tag, message) LOG_WRITE_TAG(LOG_LEVEL_ERROR, tag, message)
+#else
+#define LOG_ERROR(tag, message)
+#endif
+
+// Without tag
+
+#if LOG_LEVEL >= LOG_LEVEL_DEBUG
+#define LOG_D(message) LOG_WRITE(LOG_LEVEL_DEBUG, message)
+#else
+#define LOG_D(message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_INFO
+#define LOG_I(message) LOG_WRITE(LOG_LEVEL_INFO, message)
+#else
+#define LOG_D(message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_NOTICE
+#define LOG_N(message) LOG_WRITE(LOG_LEVEL_NOTICE, message)
+#else
+#define LOG_N(message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_WARNING
+#define LOG_W(message) LOG_WRITE(LOG_LEVEL_WARNING, message)
+#else
+#define LOG_W(message)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_ERROR
+#define LOG_E(message) LOG_WRITE(LOG_LEVEL_ERROR, message)
+#else
+#define LOG_E(message)
+#endif
