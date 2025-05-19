100897a5c3b97b9fc3374c6d0835f91a8c922786
Riley Cornelius <54592152+RileyCornelius@users.noreply.github.com>
2025-05-18 20:51:18 -0500

    Move logger functions into .cpp file

---
+++ lib/Logger/Logger.cpp
@@ -0,0 +1,116 @@
+#include "log.h"
+
+namespace _logger
+{
+#if LOG_LEVEL > LOG_LEVEL_DISABLE && LOG_PRINT_TYPE == LOG_PRINT_TYPE_PRINTF
+    void vprintf(const char *format, va_list arg)
+    {
+        char buf[64];
+        char *temp = buf;
+        va_list copy;
+        va_copy(copy, arg);
+        int len = vsnprintf(temp, sizeof(buf), format, copy);
+        va_end(copy);
+        if (len < 0)
+        {
+            va_end(arg);
+            return;
+        }
+        if (len >= (int)sizeof(buf))
+        {
+            temp = (char *)malloc(len + 1);
+            if (temp == NULL)
+            {
+                va_end(arg);
+                return;
+            }
+            len = vsnprintf(temp, len + 1, format, arg);
+        }
+        va_end(arg);
+        LOG_OUTPUT.print(temp);
+        if (temp != buf)
+        {
+            free(temp);
+        }
+    }
+
+    void printf(const char *format, ...)
+    {
+        va_list arg;
+        va_start(arg, format);
+        vprintf(format, arg);
+        va_end(arg);
+    }
+#endif // LOG_LEVEL > LOG_LEVEL_DISABLE
+
+#if LOG_TIME != LOG_TIME_DISABLE
+    const char *formatTime()
+    {
+#if LOG_TIME == LOG_TIME_MICROS
+        static char timeFormat[12];
+        sprintf(timeFormat, "%11lu", micros());
+#elif LOG_TIME == LOG_TIME_MILLIS
+        static char timeFormat[9];
+        sprintf(timeFormat, "%8lu", millis());
+#elif (LOG_TIME == LOG_TIME_HHMMSSMS) || (LOG_TIME == LOG_TIME_HHHHMMSSMS)
+        unsigned long ms = millis();
+        unsigned long seconds = ms / 1000;
+        unsigned long minutes = seconds / 60;
+        unsigned long hours = minutes / 60;
+#if LOG_TIME == LOG_TIME_HHMMSSMS
+        static char timeFormat[13];
+        sprintf(timeFormat, "%02lu:%02lu:%02lu:%03lu", hours % 24, minutes % 60, seconds % 60, ms % 1000);
+#elif LOG_TIME == LOG_TIME_HHHHMMSSMS
+        static char timeFormat[15];
+        sprintf(timeFormat, "%04lu:%02lu:%02lu:%03lu", hours, minutes % 60, seconds % 60, ms % 1000);
+#endif // LOG_TIME == LOG_TIME_HHMMSSMS
+#else
+        static char timeFormat[1] = {'\0'}; // Default to an empty string as a safeguard.
+#endif // LOG_TIME == LOG_TIME_MICROS
+        return timeFormat;
+    }
+#endif // LOG_TIME != LOG_TIME_DISABLE
+
+#if LOG_FILTER != LOG_FILTER_DISABLE
+    bool logFilter(const char *tag)
+    {
+        static const char *filterList[] = LOG_FILTER_LIST;
+        static const int length = sizeof(filterList) / sizeof(filterList[0]);
+
+        bool inFilter = false;
+        for (int i = 0; i < length; i++)
+        {
+            if (strcmp(filterList[i], tag) == 0)
+            {
+                inFilter = true;
+                break;
+            }
+        }
+        return LOG_FILTER ? inFilter : !inFilter; // INCLUDE or EXCLUDE
+    }
+#endif // LOG_LOG_FILTER != LOG_FILTER_DISABLE
+
+#if LOG_FILENAME == LOG_FILENAME_ENABLE
+    const char *filepathToName(const char *path)
+    {
+        size_t i = 0;
+        size_t pos = 0;
+        char *p = (char *)path;
+        while (*p)
+        {
+            i++;
+            if (*p == '/' || *p == '\\')
+            {
+                pos = i;
+            }
+            if (*p == '.')
+            {
+                *p = '\0';
+                break;
+            }
+            p++;
+        }
+        return path + pos;
+    }
+#endif // LOG_FILENAME == LOG_FILENAME_ENABLE
+}
--- lib/Logger/Logger.h
+++ lib/Logger/Logger.h
@@ -1,3 +1,7 @@
+/**--------------------------------------------------------------------------------------
+ ** Don't include this file. Include log.h instead.
+ *-------------------------------------------------------------------------------------*/
+
 #pragma once
 
 #include <Arduino.h>
@@ -13,7 +17,7 @@
 #define LOG_LEVEL_DEBUG 4
 #define LOG_LEVEL_VERBOSE 5
 
-#define LOG_LEVEL_TEXT_FORMAT_SINGLE 0
+#define LOG_LEVEL_TEXT_FORMAT_LETTER 0
 #define LOG_LEVEL_TEXT_FORMAT_SHORT 1
 #define LOG_LEVEL_TEXT_FORMAT_FULL 2
 
@@ -50,7 +54,7 @@
 #endif
 
 #ifndef LOG_LEVEL_TEXT_FORMAT
-#define LOG_LEVEL_TEXT_FORMAT LOG_LEVEL_TEXT_FORMAT_SINGLE
+#define LOG_LEVEL_TEXT_FORMAT LOG_LEVEL_TEXT_FORMAT_SHORT
 #endif
 
 #ifndef LOG_COLORS
@@ -84,7 +88,7 @@
  *-------------------------------------------------------------------------------------*/
 
 static_assert(LOG_LEVEL >= LOG_LEVEL_DISABLE && LOG_LEVEL <= LOG_LEVEL_VERBOSE, "LOG_LEVEL must be between LOG_LEVEL_DISABLE and LOG_LEVEL_VERBOSE");
-static_assert(LOG_LEVEL_TEXT_FORMAT >= LOG_LEVEL_TEXT_FORMAT_SINGLE && LOG_LEVEL_TEXT_FORMAT <= LOG_LEVEL_TEXT_FORMAT_FULL, "LOG_LEVEL_TEXT_FORMAT must be either LOG_LEVEL_TEXT_FORMAT_SINGLE, LOG_LEVEL_TEXT_FORMAT_SHORT or LOG_LEVEL_TEXT_FORMAT_LONG");
+static_assert(LOG_LEVEL_TEXT_FORMAT >= LOG_LEVEL_TEXT_FORMAT_LETTER && LOG_LEVEL_TEXT_FORMAT <= LOG_LEVEL_TEXT_FORMAT_FULL, "LOG_LEVEL_TEXT_FORMAT must be either LOG_LEVEL_TEXT_FORMAT_LETTER, LOG_LEVEL_TEXT_FORMAT_SHORT or LOG_LEVEL_TEXT_FORMAT_LONG");
 static_assert(LOG_TIME >= LOG_TIME_DISABLE && LOG_TIME <= LOG_TIME_HHHHMMSSMS, "LOG_TIME must be between LOG_TIME_DISABLE and LOG_TIME_HHHHMMSSMS");
 static_assert(LOG_FILTER >= LOG_FILTER_DISABLE && LOG_FILTER <= LOG_FILTER_INCLUDE, "LOG_FILTER must be between LOG_FILTER_DISABLE and LOG_FILTER_INCLUDE");
 static_assert(LOG_PRINT_TYPE >= LOG_PRINT_TYPE_PRINTF || LOG_PRINT_TYPE <= LOG_PRINT_TYPE_FMT_FORMAT, "LOG_PRINT_TYPE must be either LOG_PRINT_TYPE_PRINTF, LOG_PRINT_TYPE_STD_FORMAT or LOG_PRINT_TYPE_FMT_FORMAT");
@@ -97,118 +101,21 @@ static_assert(LOG_COLORS == LOG_COLORS_DISABLE || LOG_COLORS == LOG_COLORS_ENABL
 namespace _logger
 {
 #if LOG_LEVEL > LOG_LEVEL_DISABLE && LOG_PRINT_TYPE == LOG_PRINT_TYPE_PRINTF
-    void vprintf(const char *format, va_list arg)
-    {
-        char buf[64];
-        char *temp = buf;
-        va_list copy;
-        va_copy(copy, arg);
-        int len = vsnprintf(temp, sizeof(buf), format, copy);
-        va_end(copy);
-        if (len < 0)
-        {
-            va_end(arg);
-            return;
-        }
-        if (len >= (int)sizeof(buf))
-        {
-            temp = (char *)malloc(len + 1);
-            if (temp == NULL)
-            {
-                va_end(arg);
-                return;
-            }
-            len = vsnprintf(temp, len + 1, format, arg);
-        }
-        va_end(arg);
-        LOG_OUTPUT.print(temp);
-        if (temp != buf)
-        {
-            free(temp);
-        }
-    }
-
-    void printf(const char *format, ...)
-    {
-        va_list arg;
-        va_start(arg, format);
-        vprintf(format, arg);
-        va_end(arg);
-    }
-#endif // LOG_LEVEL > LOG_LEVEL_DISABLE
+    void vprintf(const char *format, va_list arg);
+    void printf(const char *format, ...);
+#endif
 
 #if LOG_TIME != LOG_TIME_DISABLE
-    const char *formatTime()
-    {
-#if LOG_TIME == LOG_TIME_MICROS
-        static char timeFormat[12];
-        sprintf(timeFormat, "%11lu", micros());
-#elif LOG_TIME == LOG_TIME_MILLIS
-        static char timeFormat[9];
-        sprintf(timeFormat, "%8lu", millis());
-#elif (LOG_TIME == LOG_TIME_HHMMSSMS) || (LOG_TIME == LOG_TIME_HHHHMMSSMS)
-        unsigned long ms = millis();
-        unsigned long seconds = ms / 1000;
-        unsigned long minutes = seconds / 60;
-        unsigned long hours = minutes / 60;
-#if LOG_TIME == LOG_TIME_HHMMSSMS
-        static char timeFormat[13];
-        sprintf(timeFormat, "%02lu:%02lu:%02lu:%03lu", hours % 24, minutes % 60, seconds % 60, ms % 1000);
-#elif LOG_TIME == LOG_TIME_HHHHMMSSMS
-        static char timeFormat[15];
-        sprintf(timeFormat, "%04lu:%02lu:%02lu:%03lu", hours, minutes % 60, seconds % 60, ms % 1000);
-#endif // LOG_TIME == LOG_TIME_HHMMSSMS
-
-#else
-        static char timeFormat[1] = {'\0'}; // Default to an empty string as a safeguard.
-#endif // LOG_TIME == LOG_TIME_MICROS
-
-        return timeFormat;
-    }
-#endif // LOG_TIME != LOG_TIME_DISABLE
+    const char *formatTime();
+#endif
 
 #if LOG_FILTER != LOG_FILTER_DISABLE
-    bool logFilter(const char *tag)
-    {
-        static const char *filterList[] = LOG_FILTER_LIST;
-        static const int length = sizeof(filterList) / sizeof(filterList[0]);
-
-        bool inFilter = false;
-        for (int i = 0; i < length; i++)
-        {
-            if (strcmp(filterList[i], tag) == 0)
-            {
-                inFilter = true;
-                break;
-            }
-        }
-        return LOG_FILTER ? inFilter : !inFilter; // INCLUDE or EXCLUDE
-    }
-#endif // LOG_LOG_FILTER != LOG_FILTER_DISABLE
+    bool logFilter(const char *tag);
+#endif
 
 #if LOG_FILENAME == LOG_FILENAME_ENABLE
-    const char *filepathToName(const char *path)
-    {
-        size_t i = 0;
-        size_t pos = 0;
-        char *p = (char *)path;
-        while (*p)
-        {
-            i++;
-            if (*p == '/' || *p == '\\')
-            {
-                pos = i;
-            }
-            if (*p == '.')
-            {
-                *p = '\0';
-                break;
-            }
-            p++;
-        }
-        return path + pos;
-    }
-#endif // LOG_FILENAME == LOG_FILENAME_ENABLE
+    const char *filepathToName(const char *path);
+#endif
 }
 
 /**--------------------------------------------------------------------------------------
@@ -258,7 +165,7 @@ namespace _logger
 
 // Log level text
 
-#if LOG_LEVEL_TEXT_FORMAT == LOG_LEVEL_TEXT_FORMAT_SINGLE
+#if LOG_LEVEL_TEXT_FORMAT == LOG_LEVEL_TEXT_FORMAT_LETTER
 #define _LOG_LEVEL_VERBOSE_TEXT "[V]"
 #define _LOG_LEVEL_DEBUG_TEXT "[D]"
 #define _LOG_LEVEL_INFO_TEXT "[I]"
@@ -276,7 +183,7 @@ namespace _logger
 #define _LOG_LEVEL_INFO_TEXT "[INFO]"
 #define _LOG_LEVEL_WARNING_TEXT "[WARNING]"
 #define _LOG_LEVEL_ERROR_TEXT "[ERROR]"
-#endif // LOG_LEVEL_TEXT_FORMAT == LOG_LEVEL_TEXT_FORMAT_SINGLE
+#endif // LOG_LEVEL_TEXT_FORMAT == LOG_LEVEL_TEXT_FORMAT_LETTER
 
 // Preamble format
 
@@ -291,9 +198,9 @@ namespace _logger
 #define __LOG_FILE_FORMAT(loglevel, color, format) color loglevel "[{}:{}] " format _LOG_RESET_COLOR LOG_EOL, _logger::filepathToName(__FILE__), __LINE__
 #define __LOG_TIME_FILE_FORMAT(loglevel, color, format) color "[{}]" loglevel "[{}:{}] " format _LOG_RESET_COLOR LOG_EOL, _logger::formatTime(), _logger::filepathToName(__FILE__), __LINE__
 #else
-#define __LOG_TAG_FORMAT(loglevel, color, tag, format) color "[%s]" loglevel " " format _LOG_RESET_COLOR LOG_EOL, tag
+#define __LOG_TAG_FORMAT(loglevel, color, tag, format) color loglevel "[%s] " format _LOG_RESET_COLOR LOG_EOL, tag
 #define __LOG_TAG_TIME_FORMAT(loglevel, color, tag, format) color "[%s]" loglevel "[%s] " format _LOG_RESET_COLOR LOG_EOL, _logger::formatTime(), tag
-#define __LOG_TAG_FILE_FORMAT(loglevel, color, tag, format) color loglevel "[%s:%d] " format _LOG_RESET_COLOR LOG_EOL, tag, _logger::filepathToName(__FILE__), __LINE__
+#define __LOG_TAG_FILE_FORMAT(loglevel, color, tag, format) color loglevel "[%s][%s:%d] " format _LOG_RESET_COLOR LOG_EOL, tag, _logger::filepathToName(__FILE__), __LINE__
 #define __LOG_TAG_TIME_FILE_FORMAT(loglevel, color, tag, format) color "[%s]" loglevel "[%s][%s:%d] " format _LOG_RESET_COLOR LOG_EOL, _logger::formatTime(), tag, _logger::filepathToName(__FILE__), __LINE__
 
 #define __LOG_FORMAT(loglevel, color, format) color loglevel " " format _LOG_RESET_COLOR LOG_EOL
